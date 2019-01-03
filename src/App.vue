1acb40f4bf0fc488c78d6469af7dca0e751fdb1e
Ronan <rsf.sho@gmail.com>
2019-01-03 12:33:16 -0600

    version 1 of legacy

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="body">
+    <nav>
+      <img src="@/assets/graphiteWriter.svg" alt="Graphite Writer">
+      <a href="https://beta.graphitewriter.com/#pricing">Pricing</a>
+      <a href="https://beta.graphitewriter.com/#about">About</a>
+    </nav>
+    <main id="app">
+      <div class="container">
+        <div class="versionBox">
+          <h1>Graphite Writer v2 (BETA)</h1>
+          <p>Graphite Writer v2 was designed to allow for realtime sharing between Graphite Writer users. It was
+            discontinued when we realized that our users cared more about speed and note taking than realtime sharing.</p>
+          <a href="https://v2.legacy.graphitewriter.com">Launch</a>
+        </div>
+        <div class="versionBox">
+          <h1>Graphite Writer v2 (BETA)</h1>
+          <p>Graphite Writer v2 was designed to allow for realtime sharing between Graphite Writer users. It was
+            discontinued when we realized that our users cared more about speed and note taking than realtime sharing.</p>
+          <a href="https://v1.legacy.graphitewriter.com">Launch</a>
+        </div>
+        <div class="versionThree">
+          <div>
+            <img src="@/assets/graphiteWriter.svg" alt="Graphite Writer">
+            <p>Faster Note Taking For Students</p>
+          </div>
+
+          <a href="https://beta.graphitewriter.com" class="yellow">Start Now</a>
+        </div>
+      </div>
+
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import 'minireset.css'
+  import '@/assets/main.scss'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
   }
-}
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
